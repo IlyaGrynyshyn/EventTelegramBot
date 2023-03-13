58f1f36e0a6e9dbbe68c58b46c9ee3b5a8943c21
Ilya Grynyshyn <grynyshyn080@gmail.com>
2023-03-13 22:53:31 +0200

    change text

--- handlers/users/about_us.py
+++ handlers/users/about_us.py
@@ -7,6 +7,23 @@ from loader import dp
 
 @dp.message_handler(text='📦 Про івент')
 async def bot_help(message: types.Message):
-    text = "Про івент"
+    text = """
+    *The Power of Identity* - захід created by ALENA MAER, з метою занурити вас у світ ….
+.
+.
+.
+.
+.
+
+Agenda заходу буде такою:
+
+-  Networking space  
+- Панельна Дискусія з ученицями попередніх потоків Наставництва 
+- Alena’s speech 
+- Music/Photo/Beauty time 
+
+
+✨З нетерпінням чекаємо зустріти саме тебе 29го квітня о 13:00 в KameLotHub✨
+    """
 
     await message.answer(text, reply_markup=menu)
